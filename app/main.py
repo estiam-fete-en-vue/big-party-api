7a075973fd45e2e9cbb05a5a8219dde5eb46b915
faraan-estiam <faraan.rozbully@estiam.com>
2025-04-03 17:01:38 +0200

    added basic payments

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
-from .routers import default, auth
+from .routers import default, auth, payments
 
 
-app = FastAPI()
+app = FastAPI(
+  redoc_url="/",
+  docs_url=None
+)
 
 app.include_router(auth.router)
+app.include_router(payments.router)
 app.include_router(default.router)
